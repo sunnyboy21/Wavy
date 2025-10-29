fd5f3788ef70e2ac80c4d3bbf650ba3ea4f682e8
sunnyboy21 <fheimbac@smail.uni-koeln.de>
2025-10-29 15:31:49 +0100

    h2 h3

---
+++ src/h3/H3_main.java
@@ -0,0 +1,13 @@
+package h3;
+
+public class H3_main {
+
+	public static void main(String args[]) {
+		int z = 11;
+		// System.out.println(z);
+		if (z > 10) {
+			z = z - 10;
+		}
+		System.out.println(z);
+	}
+}
