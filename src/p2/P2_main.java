614a55b37317820d9f96ee3efd98d4b571fc3dce
sunnyboy21 <fheimbac@smail.uni-koeln.de>
2025-10-31 11:24:02 +0100

    gg

---
+++ src/p2/P2_main.java
@@ -0,0 +1,18 @@
+package p2;
+
+public class P2_main {
+
+	public static void main(String[] args) {
+
+		int i = 9;
+		// System.out.println(i);
+
+		boolean ausdruckb = false;
+
+		if (i > 10)
+			ausdruckb = true;
+
+		System.out.println(ausdruckb);
+	}
+
+}
