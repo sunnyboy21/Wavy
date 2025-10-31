1f047c40dd0c80d8b9e645f6fcf47ea2916f2f09
sunnyboy21 <fheimbac@smail.uni-koeln.de>
2025-10-31 10:34:06 +0100

    Aufgabe h3

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -3,11 +3,39 @@ package h3;
 public class H3_main {
 
 	public static void main(String args[]) {
-		int z = 11;
+		
+		int i=8, j=6, k=-10;
 		// System.out.println(z);
-		if (z > 10) {
-			z = z - 10;
-		}
-		System.out.println(z);
+		
+		boolean ausdruck1,ausdruck2,ausdruck3;
+		
+		if (i>j)
+			ausdruck1= true;
+		else
+			ausdruck1 = false;
+		
+		if (i>200)
+			ausdruck2= true;
+		else
+			ausdruck2 = false;
+		
+		if (j>100)
+			ausdruck3= true;
+		else
+			ausdruck3 = false;
+		
+		if (ausdruck1 & ausdruck2 & ausdruck3) 
+			k=3;
+		
+		if (ausdruck1 & ausdruck2 & !ausdruck3) 
+			k=2;
+		
+		if (ausdruck1 & !ausdruck2 & !ausdruck3) 
+			k=1;
+		
+		if (!ausdruck1 & !ausdruck2 & !ausdruck3) 
+			k=4;
+		
+		System.out.println((k ));
 	}
 }
